d13ecbf555d58d4dbf7fd90553362882dd0537ac
MatthewLavoie <127175685+MatthewRLavoie@users.noreply.github.com>
2025-02-12 14:18:58 -0500

    Update HW2.go

--- HW2.go
+++ HW2.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"fmt" // Print results
+	"time" // Time delay to prevent incorrect order of goroutine execution
 )
 
+// Producer goroutine
 func producer(ch chan int) {
+	//increamental counter
 	for i := 1; i <= 5; i++ {
 		fmt.Printf("Producer: %d\n", i)
 		ch <- i
@@ -14,14 +16,16 @@ func producer(ch chan int) {
 	close(ch) // Close channel to signal completion
 }
 
+//consumer goroutine
 func consumer(ch chan int) {
 	for num := range ch {
 		fmt.Printf("Consumer: %d\n", num)
 	}
 }
 
+//main execution
 func main() {
 	ch := make(chan int) // Unbuffered channel for communication
 	go producer(ch)
-	consumer(ch) // Running consumer in the main Goroutine
+	consumer(ch) // Running consumer in the main goroutine
 }
